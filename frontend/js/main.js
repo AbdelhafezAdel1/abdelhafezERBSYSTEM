e3b44727e7f7a3cc6c5d6a969159f291be60f9f7
AbdelhafezAdel1 <abdelhafezadel.js@gmail.com>
2026-02-19 05:39:56 +0200

    تعديل جذري للمسافات ورفع قسم البنك والإجمالي للأعلى

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1109,7 +1109,7 @@ async function viewInvoice(id) {
                         </div>
 
                         <!-- Footer Section: Totals & Bank -->
-                        <div class="mt-6 relative z-20">
+                        <div class="mt-[150px] relative z-20">
                             <div class="flex gap-[32px] items-start">
                                 <!-- Notes & Terms -->
                                 <div class="w-1/2 pt-1">
